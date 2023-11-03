c25c86457966c52bde3b2f4e809020f8e69a9c12
CiprianNica <ciprian.nica77@gmail.com>
2023-11-03 07:50:09 +0100

    ej 5

---
+++ ejercicios/ejercicio5.py
@@ -0,0 +1,8 @@
+"""
+cuanto es el X por ciento de Y numero
+"""
+
+numero = int(input('introd el numero: '))
+porcentaje = int(input('que porcentaje quieres sacar del numero ? '))
+result = porcentaje * numero / 100
+print(result)
