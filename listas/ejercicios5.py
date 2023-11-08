563de12f291941bc524fa3c0c2663723d23ff641
CiprianNica <ciprian.nica77@gmail.com>
2023-11-08 21:24:44 +0100

    lista ejer 5

---
+++ listas/ejercicios5.py
@@ -0,0 +1,9 @@
+"""
+crear una lista con el contenido de esta tabla:
+accion:        aventura                deportes
+gta              assins                  fifa21
+cod              crash                   pro21
+pugb             princd of persia        motogp 21
+
+mostrar esta inf ordenada
+"""
