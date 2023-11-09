5de504a0cc4f8cda8259bb1e3822a2ec1e6c2e4f
CiprianNica <ciprian.nica77@gmail.com>
2023-11-09 22:54:37 +0100

    listas - ej5

--- listas/ejercicios5.py
+++ listas/ejercicios5.py
@@ -1,9 +1,29 @@
 """
 crear una lista con el contenido de esta tabla:
-accion:        aventura                deportes
-gta              assins                  fifa21
+accion:        aventura:               deportes:
+gta              asasins                  fifa21
 cod              crash                   pro21
 pugb             princd of persia        motogp 21
 
 mostrar esta inf ordenada
 """
+
+tabla = [
+    {
+        'categoria': 'ACCION',
+        'juegos': ['GTA', 'COD', 'PUGB']
+    },
+    {
+        'categoria': 'AVENTURA',
+        'juegos': ['ASASINS', 'CRASH', 'PRINCE OF PERSIA']
+    },
+    {
+        'categoria': 'DEPORTTES',
+        'juegos': ['FIFA21', 'PRO21', 'MOTOGP21']
+    }
+]
+
+for categoria in tabla:
+    print(f"-------------------{categoria['categoria']}--------------------")
+    for juego in categoria['juegos']:
+        print(juego)
